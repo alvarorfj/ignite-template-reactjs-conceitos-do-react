1ea0bb48267919c6d6825516245e84013798b722
Alvaro <alvarorfj@gmail.com>
2022-07-26 22:44:12 -0300

    Implementando os metodos solicitados no desafio do ignite-rockeseat

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,25 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle){
+      var newTask: Task = {
+        id: Math.floor(Math.random() * 10000),
+        title: newTaskTitle,
+        isComplete: false,
+      }
+      setTasks([...tasks, newTask]);
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    tasks.filter(x => x.id === id).forEach(x => x.isComplete = !x.isComplete);
+    setTasks([...tasks]);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks([...tasks.filter(x => x.id !== id)])
   }
 
   return (
